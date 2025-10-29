bd3f3e26e0b9eb2e547b8b998da00f9ca7114a46
Minami Ueda <minami.ueda@gmail.com>
2025-10-29 18:01:40 -0400

    Use GCS to store terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 1.5.0"
+
+  backend "gcs" {
+    bucket = "gs_mobs_jessica"
+    prefix = "pipeline-terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
